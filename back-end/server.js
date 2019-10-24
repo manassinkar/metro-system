8d8df50c2ddde5e738ef8710ffa0661420658c76
Manas Sinkar <manas.sinkar@gmail.com>
2019-10-24 20:50:57 +0530

    First Commit

---
+++ back-end/server.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let app = express();
+let data = require('./data');
+
+let CONNECTION_URL = data.cloudDatabaseURL;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { mongoose.connect(CONNECTION_URL, { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true },(err) =>
+{
+    if(err)
+    {
+        console.log('Database Not Connected',err);
+    }
+    else
+    {
+        console.log('Database Connected Successfully and Back End Listening on Port',PORT);
+    }
+});});
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
