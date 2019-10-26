67c6a6261c79460efd6c5fe25409b69788fedea7
Manas Sinkar <manas.sinkar@gmail.com>
2019-10-26 16:53:41 +0530

    Minor bug handled

--- back-end/routes/coupon.route.js
+++ back-end/routes/coupon.route.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var cC = require('../controllers/coupon.controller');
 
 router.post('/addCoupon',cC.addCoupon);
-router.post('/applyCoupon',pC.applyCoupon);
+router.post('/applyCoupon',cC.applyCoupon);
 
 module.exports = router;
